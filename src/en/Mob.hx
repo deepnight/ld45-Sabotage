6808939bf092a6d57b265e280527b6eecbc69cc6
Sébastien Bénard <fork@deepnight.net>
2019-10-05 17:20:49 +0200

    Basic mob alarm mechanic

--- src/en/Mob.hx
+++ src/en/Mob.hx
@@ -4,6 +4,7 @@ class Mob extends Entity {
 	public static var ALL : Array<Mob> = [];
 
 	var patrolPts : Array<CPoint> = [];
+	var lastAlarmPt : CPoint;
 	var curPatrolIdx = 0;
 	var curPatrolPt(get,never) : CPoint; inline function get_curPatrolPt() return patrolPts[curPatrolIdx];
 
@@ -13,13 +14,15 @@ class Mob extends Entity {
 	public function new(x,y, data:ogmo.Entity) {
 		super(x,y);
 		ALL.push(this);
+		lastAlarmPt = new CPoint(cx,cy);
+
+		// Parse patrol
 		for(n in data.nodes) {
 			for(pt in patrolPts)
 				if( pt.is(n.cx,n.cy) )
 					throw "Duplicate patrol point for mob at "+cx+","+cy;
 			patrolPts.push( new CPoint(n.cx, n.cy) );
 		}
-
 		if( patrolPts.length==0 || !patrolPts[0].is(cx,cy) )
 			patrolPts.insert(0, new CPoint(cx,cy));
 
@@ -44,14 +47,15 @@ class Mob extends Entity {
 		viewCone.y = footY;
 		viewCone.visible = !level.hasVisibleRoof(cx,cy);
 
-		viewCone.scaleX += ( ( hasAlarm() ? sightCheckEnt(hero) ? 0.8 : 0.5 : 0.25 ) - viewCone.scaleX ) * 0.2;
-		viewCone.scaleY = viewCone.scaleX;
+		viewCone.scaleX += ( ( hasAlarm() && sightCheckEnt(hero) ? 0.5 : 0.3 ) - viewCone.scaleX ) * 0.2;
+		viewCone.scaleY += ( ( hasAlarm() && sightCheckEnt(hero) ? 0.2 : 0.3 ) - viewCone.scaleY ) * 0.2;
 		viewCone.alpha += ( ( hasAlarm() ? 0.3 : 0.5 ) - viewCone.alpha ) * 0.2;
-		if( !hasAlarm() || !sightCheckEnt(hero) ) {
+		if( !hasAlarm() )
 			viewCone.rotation += M.radSubstract(lookAng,viewCone.rotation)*0.2 ;
-		}
-		else
+		else if( sightCheckEnt(hero) )
 			viewCone.rotation += M.radSubstract(angTo(hero),viewCone.rotation)*0.2 ;
+		else
+			viewCone.rotation += M.radSubstract(lookAng, viewCone.rotation ) * 0.2 ;
 		viewCone.colorize( hasAlarm() ? sightCheckEnt(hero) ? 0xff0000 : 0xffdd00 : 0x7a9aff );
 	}
 
@@ -59,25 +63,37 @@ class Mob extends Entity {
 	override function update() {
 		super.update();
 
-		if( M.dist(footX, footY, curPatrolPt.footX, curPatrolPt.footY)>Const.GRID*0.1 ) {
-			// Patrol movement
-			var s = 0.005;
-			var a = Math.atan2(curPatrolPt.footY-footY, curPatrolPt.footX-footX);
-			dx+=Math.cos(a)*s;
-			dy+=Math.sin(a)*s;
-			lookAng = M.round(a/M.PIHALF)*M.PIHALF;
-			// lookAng += M.radSubstract( M.round(a/M.PIHALF)*M.PIHALF, lookAng ) * 0.2;
+		if( !hasAlarm() ) {
+			if( M.dist(footX, footY, curPatrolPt.footX, curPatrolPt.footY)>Const.GRID*0.1 ) {
+				// Patrol movement
+				var s = 0.005;
+				var a = Math.atan2(curPatrolPt.footY-footY, curPatrolPt.footX-footX);
+				dx+=Math.cos(a)*s;
+				dy+=Math.sin(a)*s;
+				lookAng = M.round(a/M.PIHALF)*M.PIHALF;
+			}
+			else {
+				// Reached patrol point
+				curPatrolIdx++;
+				if( curPatrolIdx>=patrolPts.length )
+					curPatrolIdx = 0;
+			}
 		}
 		else {
-			// Reached patrol point
-			curPatrolIdx++;
-			if( curPatrolIdx>=patrolPts.length )
-				curPatrolIdx = 0;
+			// Track alarm source
+			var s = 0.007;
+			var a = Math.atan2(lastAlarmPt.footY-footY, lastAlarmPt.footX-footX);
+			dx+=Math.cos(a)*s;
+			dy+=Math.sin(a)*s;
+			if( distPxFree(lastAlarmPt.footX,lastAlarmPt.footY)>=Const.GRID*0.5 )
+				lookAng = a;
 		}
-		// fx.viewCone(footX, footY, lookAng);
 
-		if( sightCheckEnt(hero) )
-			cd.setS("alarm",1);
+		// See hero
+		if( sightCheckEnt(hero) && M.radDistance(angTo(hero),lookAng)<=M.PI*0.3 ) {
+			lastAlarmPt.set(hero.cx, hero.cy, hero.xr, hero.yr);
+			cd.setS("alarm",3);
+		}
 
 		// Sound emit fx
 		if( level.hasVisibleRoof(cx,cy) && !cd.hasSetS("soundFx", 1) && distCase(hero)<=10 )
