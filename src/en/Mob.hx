575cd64ebfd14be4b498a1f377744472a849a957
Sébastien Bénard <fork@deepnight.net>
2019-10-05 16:46:47 +0200

    Cleanup

--- src/en/Mob.hx
+++ src/en/Mob.hx
@@ -1,7 +1,6 @@
 package en;
 
 class Mob extends Entity {
-	// var targetPt : CPoint;
 	var patrolPts : Array<CPoint> = [];
 	var curPatrolIdx = 0;
 	var curPatrolPt(get,never) : CPoint; inline function get_curPatrolPt() return patrolPts[curPatrolIdx];
@@ -9,10 +8,12 @@ class Mob extends Entity {
 	public function new(x,y, data:ogmo.Entity) {
 		super(x,y);
 		for(n in data.nodes) {
-			// if( n.cx==cx && n.cy==cy )
-			// 	continue;
+			for(pt in patrolPts)
+				if( pt.is(n.cx,n.cy) )
+					throw "Duplicate patrol point for mob at "+cx+","+cy;
 			patrolPts.push( new CPoint(n.cx, n.cy) );
 		}
+
 		if( patrolPts.length==0 || !patrolPts[0].is(cx,cy) )
 			patrolPts.insert(0, new CPoint(cx,cy));
 	}
@@ -20,11 +21,8 @@ class Mob extends Entity {
 	override function update() {
 		super.update();
 
-		// Patrol
-		// for(pt in patrolPts)
-		// 	fx.markerCase(pt.cx, pt.cy, 0.1, 0xff0000);
-		// fx.markerCase(curPatrolPt.cx, curPatrolPt.cy, 0.1, 0xff00ff);
 		if( M.dist(footX, footY, curPatrolPt.footX, curPatrolPt.footY)>Const.GRID*0.1 ) {
+			// Patrol movement
 			var s = 0.005;
 			var a = Math.atan2(curPatrolPt.footY-footY, curPatrolPt.footX-footX);
 			dx+=Math.cos(a)*s;
@@ -37,6 +35,7 @@ class Mob extends Entity {
 				curPatrolIdx = 0;
 		}
 
+		// Sound emit fx
 		if( level.hasVisibleRoof(cx,cy) && !cd.hasSetS("soundFx", 1) )
 			fx.emitSound(footX, footY);
 	}
