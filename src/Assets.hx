1aae9f410f9c1118d657e5dd0f1c3d03c28be7b0
Sébastien Bénard <fork@deepnight.net>
2019-10-18 15:31:27 +0200

    Cherry pick music update

--- src/Assets.hx
+++ src/Assets.hx
@@ -9,7 +9,7 @@ class Assets {
 	public static var fontMedium : h2d.Font;
 	public static var fontLarge : h2d.Font;
 	public static var tiles : SpriteLib;
-	static var music : hxd.res.Sound;
+	static var music : dn.heaps.Sfx;
 
 	static var initDone = false;
 	public static function init() {
@@ -20,11 +20,12 @@ class Assets {
 		dn.heaps.Sfx.muteGroup(0); // HACK
 		dn.heaps.Sfx.setGroupVolume(0,0.6);
 
-		#if hl
-		music = hxd.Res.music.leatherneck_hl;
+		#if js
+		music = new dn.heaps.Sfx(hxd.Res.music.leatherneck_js);
 		#else
-		music = hxd.Res.music.leatherneck;
+		music = new dn.heaps.Sfx(hxd.Res.music.leatherneck_hl);
 		#end
+		music.groupId = 1;
 
 		fontPixel = hxd.Res.fonts.minecraftiaOutline.toFont();
 		fontTiny = hxd.Res.fonts.barlow_condensed_medium_regular_9.toFont();
@@ -49,9 +50,8 @@ class Assets {
 		tiles.defineAnim("bird", "0(2), 1(2), 2(4), 4(2), 3(6), 5(1)");
 	}
 
-	static var chan : Null<hxd.snd.Channel>;
 	public static function playMusic() {
-		chan = music.play(true, 0.75);
+		music.play(true);
 	}
 	public static function stopMusic() {
 		if( chan!=null ) {
@@ -60,9 +60,6 @@ class Assets {
 		}
 	}
 	public static function toggleMusicPause() {
-		if( chan!=null )
-			stopMusic();
-		else
-			playMusic();
+		music.togglePlay(true);
 	}
 }
