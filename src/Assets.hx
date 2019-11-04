d5e84789f40bd8bdfcd0b57e171e7d8dbd189ace
Sébastien Bénard <fork@deepnight.net>
2019-11-04 10:32:03 +0100

    Updated for latest libs

--- src/Assets.hx
+++ src/Assets.hx
@@ -1,7 +1,7 @@
 import dn.heaps.slib.*;
 
 class Assets {
-	public static var SFX = dn.heaps.Sfx.importDirectory("sfx");
+	public static var SFX = dn.heaps.assets.SfxDirectory.load("sfx");
 
 	public static var fontPixel : h2d.Font;
 	public static var fontTiny : h2d.Font;
@@ -29,7 +29,7 @@ class Assets {
 		fontMedium = hxd.Res.fonts.barlow_condensed_medium_regular_17.toFont();
 		fontLarge = hxd.Res.fonts.barlow_condensed_medium_regular_32.toFont();
 
-		tiles = dn.heaps.slib.assets.Atlas.load("atlas/tiles.atlas");
+		tiles = dn.heaps.assets.Atlas.load("atlas/tiles.atlas");
 		tiles.defineAnim("heroRun", "0(2), 1(1), 2(2), 1(1)");
 		tiles.defineAnim("heroIdle", "0(10), 1(1), 2(8)");
 		tiles.defineAnim("heroThrow", "0(1), 1(999)");
