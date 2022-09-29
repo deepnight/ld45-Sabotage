57dfcc6c00a30df93c470e54e3e627a0513f40ac
Sebastien Benard <github@deepnight.net>
2022-09-29 11:51:42 +0200

    Fixed LangParser

--- src.langParser/LangParser.hx
+++ src.langParser/LangParser.hx
@@ -4,13 +4,9 @@ class LangParser {
 	public static function main() {
 		var name = "sourceTexts";
 		Sys.println("Building "+name+" file...");
-		var data = GetText.doParseGlobal({
-			codePath: "src",
-			codeIgnore: null,
-			cdbFiles: [],
-			cdbSpecialId: [],
-			potFile: "res/lang/"+name+".pot",
-		});
+		var entries = [];
+		entries = entries.concat( GetText.parseSourceCode("src") );
+		GetText.writePOT('res/lang/$name.pot', entries);
 		Sys.println("Done.");
 	}
 }
